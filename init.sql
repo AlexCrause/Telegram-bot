bbb1fb9affdba227f63a92f7a5853bac6a9c438c
AlexeiHozyashev <crause937@gmail.com>
2026-04-13 20:19:24 +0500

    changed the way tables are created

--- init.sql
+++
@@ -1,5 +0,0 @@
-create table if not exists subscribers (
-    user_id uuid primary key,
-    user_id_telegram bigint not null unique,
-    price bigint
-);
---
+++ src/main/java/com/skillbox/cryptobot/model/Subscribers.java
@@ -0,0 +1,29 @@
+package com.skillbox.cryptobot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "subscribers")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Subscribers {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "user_id", nullable = false)
+    private UUID id;
+
+    @Column(name = "user_id_telegram", nullable = false, unique = true)
+    private Long userIdTelegram;
+
+    @Column(name="price_crypto")
+    private Long priceCrypto;
+}
